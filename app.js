ee7dad516c4f91fd5fff9793c33fcceaf2b34733
CamiSarmiento <sarmiento_camila@hotmail.com>
2023-11-13 23:07:17 -0300

    salve

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
+    faq.classList.remove('active');
     
 }
 
@@ -11,12 +12,21 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
-    
+    faq.classList.remove('active');
 }
 
+let faq= document.querySelector('.faq-info');
+
+document.querySelector('#info-btn').onclick = () =>{
+    faq.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    
+}
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
+    faq.classList.remove('active');
 
 }
